a85df0f54556fd2e38460cb111f089791f5cdb60
rootyss <jaxent@yandex.ru>
2021-04-28 18:29:48 +0300

    'fix_lint'

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: {
-        main: path.resolve(__dirname, './src/init.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './template.html'),
-            filename: 'index.html',
-        }),
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/init.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './template.html'),
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-}
+  },
+};
