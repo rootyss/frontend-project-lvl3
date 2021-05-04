f3bb943815958f5b6a2be3086ee6357444634ce6
rootyss <jaxent@yandex.ru>
2021-05-04 16:37:00 +0300

    'fix_template'

--- src/init.js
+++ src/init.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+
+class A {
+    constructor() {}
+    method1() {}
+    method2() { }
+    method3() { }
+};
+const funcprop = () => {
+    (Math.random());
+};
