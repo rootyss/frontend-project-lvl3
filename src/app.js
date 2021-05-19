d4ea59940014c17c2719f41e5d42fb121423f1f4
rootyss <jaxent@yandex.ru>
2021-05-19 15:39:12 +0300

    'fix_i18n'

--- src/app.js
+++ src/app.js
@@ -8,52 +8,52 @@ import state from './state.js';
 import render from './render.js';
 
 const app = () => {
-  const watchedState = onChange(state, () => {
-    render(state, i18nInstance);
-  });
-
   const i18nInstance = i18n.createInstance();
   i18nInstance
     .init({
       lng: 'ru',
       debug: false,
       resources,
-    }).then((t) => { render(watchedState, i18nInstance); });
-
-  const form = document.querySelector('form');
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    watchedState.rssForm.state = 'sending';
-
-    const formData = new FormData(event.target);
-    const inputUrl = formData.get('url');
-
-    watchedState.rssForm.errors = validate(inputUrl);
-    if (watchedState.rssForm.errors) {
-      return;
-    }
-
-    if (watchedState.rss.includes(inputUrl)) {
-      watchedState.rssForm.errors = 'RSS уже добавлен';
-      return;
-    }
-
-    watchedState.rss.push(inputUrl);
-
-    const stream = getStream(inputUrl);
-
-    stream.then((response) => {
-      const data = parse(response.data.contents);
-      addNormalizedData(data, watchedState);
-      watchedState.rssForm.state = 'finished';
-    })
-      .catch((e) => {
-        watchedState.rssForm.state = 'failed';
-        watchedState.rssForm.errors = e.message;
+    }).then(() => {
+      const watchedState = onChange(state, () => {
+        render(state, i18nInstance);
       });
-  });
-  render(watchedState, i18nInstance);
+      const form = document.querySelector('form');
+
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        watchedState.rssForm.state = 'sending';
+
+        const formData = new FormData(event.target);
+        const inputUrl = formData.get('url');
+
+        watchedState.rssForm.errors = validate(inputUrl);
+        if (watchedState.rssForm.errors) {
+          return;
+        }
+
+        if (watchedState.rss.includes(inputUrl)) {
+          watchedState.rssForm.errors = 'RSS уже добавлен';
+          return;
+        }
+
+        watchedState.rss.push(inputUrl);
+
+        const stream = getStream(inputUrl);
+
+        stream.then((response) => {
+          const data = parse(response.data.contents);
+          addNormalizedData(data, watchedState);
+          watchedState.rssForm.state = 'finished';
+        })
+          .catch((e) => {
+            watchedState.rssForm.state = 'failed';
+            watchedState.rssForm.errors = e.message;
+          });
+      });
+      render(watchedState, i18nInstance);
+    });
+  return i18nInstance;
 };
 
 app();
