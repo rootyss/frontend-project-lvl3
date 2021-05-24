e803bc1cb8a47446dd65f56588657f12f1e4bbee
rootyss <jaxent@yandex.ru>
2021-05-24 18:07:34 +0300

    'fix'

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,7 @@ const getFileName = (ext) => (isDev ? `[name].bundle.${ext}` : `[name].[contenth
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: {
-    main: './init.js',
-  },
+  entry: './init.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: getFileName('js'),
